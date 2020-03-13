876af1bc4be1623bb727d2d9be6d5bf99097254f
NuclearRedeye <j.summerfield@elifesciences.org>
2020-03-13 14:16:16 +0000

    chore: add some basic tests

--- test/monitorBucket.spec.js
+++ test/monitorBucket.spec.js
@@ -3,6 +3,7 @@ import { default as config } from './config.js';
 
 AWS.config.update(config);
 
+let terminate = false;
 const url = `https://sqs.us-east-2.amazonaws.com/540790251273/test-editor-bucket-event-queue`;
 const sqs = new AWS.SQS();
 
@@ -12,30 +13,39 @@ const params = {
   WaitTimeSeconds: 20
 };
 
-sqs.receiveMessage(params, (err, data) => {
-  if (err) {
-    console.log(`Error: ${err.stack}`);
-  } else {
-    if (data && data.Messages) {
-      console.log(`Got: ${data.Messages.length} event(s) to process`);
+async function poll(queue) {
+  const { Messages } = await sqs.receiveMessage(params).promise();
+  if (Messages && Messages.length) {
+    console.log(`Got: ${Messages.length} event(s) to process`);
 
-      for (const message of data.Messages) {
-        console.log(`Processing: ${message.MessageId}`);
-        console.log(`Event: ${message.Body}`);
+    for (const message of Messages) {
+      console.log(`Processing: ${message.MessageId}`);
+      console.log(`Event: ${message.Body}`);
 
-        const deleteParams = {
-          QueueUrl: url,
-          ReceiptHandle: message.ReceiptHandle
-        };
+      const deleteParams = {
+        QueueUrl: url,
+        ReceiptHandle: message.ReceiptHandle
+      };
 
-        sqs.deleteMessage(deleteParams, (err, data) => {
-          if (err) {
-            console.log(err, err.stack);
-          } else {
-            console.log('Successfully deleted message from queue');
-          }
-        });
-      }
+      await sqs.deleteMessage(deleteParams).promise();
     }
   }
+}
+
+let count = 0;
+async function listen() {
+  do {
+    console.log(`${++count}: Polling...`);
+    await poll();
+  } while (!terminate);
+}
+
+process.on('SIGINT', () => {
+  terminate = true;
 });
+
+process.on('SIGTERM', () => {
+  terminate = true;
+});
+
+listen();
